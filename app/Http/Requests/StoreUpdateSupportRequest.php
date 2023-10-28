b36af3f3e79ad7dd2e0b03cf370858e9181057a1
issler78 <132786398+Issler78@users.noreply.github.com>
2023-10-28 14:28:52 -0300

    19th commit - API Rest update method

--- app/Http/Requests/StoreUpdateSupportRequest.php
+++ app/Http/Requests/StoreUpdateSupportRequest.php
@@ -27,13 +27,13 @@ class StoreUpdateSupportRequest extends FormRequest
             'body' => 'required|min:3|max:10000'
         ];
 
-        if($this->method() === 'PUT')
+        if($this->method() === 'PUT' || $this->method() === 'PATCH')
         {
             $rules['subject'] = [
                 'required',
                 'min:3',
                 'max:255',
-                Rule::unique('supports')->ignore($this->id)
+                Rule::unique('supports')->ignore($this->support ?? $this->id)
             ];
         }
 
