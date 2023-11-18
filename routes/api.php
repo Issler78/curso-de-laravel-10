6ad515ee10c7c7fd678a26ef2398ef3b507b1488
issler78 <132786398+Issler78@users.noreply.github.com>
2023-11-18 15:22:58 -0300

    API Auth with Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/supports', SupportController::class);
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group( function () {
+    
+    Route::apiResource('/supports', SupportController::class);
+
+});
+
