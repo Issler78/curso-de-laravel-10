0617941127528a43ae5f98596bd55bc865a4977f
issler78 <132786398+Issler78@users.noreply.github.com>
2024-01-09 15:06:47 -0300

    create ReplySupportController + fix views + tailwind in the show/replies view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\Admin\{ReplySupportController, SupportController};
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
@@ -15,10 +15,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/supports/{id}/replies', [ReplySupportController::class, 'index'])->name('replies.index');
 
     Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
     Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
@@ -26,7 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
     Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
     Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
-    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
 });
 
 require __DIR__.'/auth.php';
