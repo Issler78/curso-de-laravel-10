92f64344ba61c897b2678c0797902d8f7c53dd54
issler78 <132786398+Issler78@users.noreply.github.com>
2024-01-16 15:54:27 -0300

    create controller api list replies of support

---
+++ app/Http/Controllers/Api/ReplySupportController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\Replies\CreateReplyDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreReplySupportRequest;
+use App\Http\Resources\ReplySupportResource;
+use App\Services\ReplySupportService;
+use App\Services\SupportService;
+use Illuminate\Http\Request;
+
+class ReplySupportController extends Controller
+{
+    public function __construct(
+        protected SupportService $Supportservice,
+        protected ReplySupportService $replyService
+    ) {}
+
+    public function getRepliesFromSupport(string $support_id)
+    {
+        if(!$this->Supportservice->findOne($support_id))
+        {
+            return response()->json(['message' => 'Not Found'], 404);
+        }
+
+        $replies = $this->replyService->getAllBySupport($support_id);
+        
+        return ReplySupportResource::collection($replies);
+    }
