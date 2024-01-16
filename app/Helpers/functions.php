953d8fcc033496d48a255ea9fb977a0b0e8133d5
issler78 <132786398+Issler78@users.noreply.github.com>
2024-01-16 14:03:30 -0300

    create (Helper) get initials for name

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -9,3 +9,23 @@ if (!function_exists('getStatusSupport'))
         return SupportStatus::fromValue($status);
     }
 }
+
+if (!function_exists('getInitials'))
+{
+    function getInitials(string $name)
+    {
+        $words = explode(' ', $name);
+        $initials = '';
+
+        foreach ($words as $word)
+        {
+            $initials .= strtoupper(substr($word, 0, 1));
+            if (strlen($initials) >= 2)
+            {
+                break;
+            }
+        }
+
+        return $initials;
+    }
+}
