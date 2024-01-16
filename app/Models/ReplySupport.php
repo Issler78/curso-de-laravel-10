d436a1ac22f1164ac57af07f9468fd494dd9824f
issler78 <132786398+Issler78@users.noreply.github.com>
2024-01-16 14:53:43 -0300

    apply anonymous global scope

--- app/Models/ReplySupport.php
+++ app/Models/ReplySupport.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -17,6 +18,13 @@ class ReplySupport extends Model
     protected $fillable = ['user_id', 'support_id', 'content'];
 
     protected $with = ['user'];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('order', function (Builder $builder) {
+            $builder->latest();
+        });
+    }
 
     protected $table = 'replies_support';
 
